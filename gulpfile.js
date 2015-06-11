1fb0aeafc4c7a9410f73e71923fbef37e8c0b78e
Jhorlin <jhorlin@gmail.com>
2015-06-11 13:16:20 -0400

    added gulp task to check that lcov exists

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,26 @@
         coveralls = require('gulp-coveralls'),
         fs = require('fs'),
         gutil = require('gulp-util'),
+        expect = require('gulp-expect-file'),
         streamProcessors,
         runSequence = require('run-sequence'),
         paths = {
             src: ['./index.js', './statusCodes.js'],
             unit: ['test/**/*.js'],
-            lcov: ['reports/coverage.lcov']
+            lcov: 'reports/coverage.lcov'
         };
+
+    function exitOnError (stream) {
+        var errored = false;
+        return stream.on('error', function (err) {
+            errored = true;
+            gutil.log(err.message);
+        }).on('end', function () {
+            if (errored) {
+                process.exit(0);
+            }
+        })
+    }
 
     gulp.task('exit', function () {
         streamProcessors.push(exit());
@@ -29,6 +42,10 @@
         streamProcessors = [gulp.src(paths.lcov)];
     });
 
+    gulp.task('expect:lcov', function(){
+       streamProcessors.push(expect(paths.lcov))
+    });
+
     gulp.task('unit', function () {
         streamProcessors = [gulp.src(paths.unit)];
     });
@@ -36,87 +53,57 @@
     gulp.task('process', function () {
         var stream = streamProcessors.shift();
         return streamProcessors.reduce(function (stream, processor) {
-            return stream.pipe(processor);
-        }, stream);
+            return stream.pipe(exitOnError(processor));
+        }, exitOnError(stream));
     });
 
     gulp.task('coveralls', function () {
-        var errored = false;
-        streamProcessors.push(coveralls()
-                .on('error', function (err) {
-                    errored = true;
-                    gutil.log("coveralls failed:", err.message);
-                })
-                .on('end', function () {
-                    if (errored) {
-                        process.exit(0);
-                    }
-                })
-        );
+        streamProcessors.push(coveralls());
     });
 
     gulp.task('mocha', function () {
-        var errored = false;
-        streamProcessors.push(mocha()
-                .on('error', function (err) {
-                    errored = true;
-                    gutil.log("mocha failed:", err.message);
-                })
-                .on('end', function () {
-                    if (errored) {
-                        process.exit(0);
-                    }
-                })
-        );
-        return streamProcessors;
+        streamProcessors.push(mocha());
     })
 
     gulp.task('instrument', function () {
-        return streamProcessors.push(cover.instrument({pattern: paths.src}));
+        streamProcessors.push(cover.instrument({pattern: paths.src}));
     });
 
 
     gulp.task('gather', function () {
-        return streamProcessors.push(cover.gather());
+        streamProcessors.push(cover.gather());
     });
 
     gulp.task('enforce', function () {
-        return streamProcessors.push(cover.enforce({
+        streamProcessors.push(cover.enforce({
             statements: 80,
             blocks: 80,
             lines: 80,
             uncovered: undefined
-        })
-            .on('error', function (err) {
-                gutil.log("enforce failed:", err.message);
-            }));
+        }));
     })
 
     gulp.task('format', function () {
-        return streamProcessors.push(cover.format([
+        streamProcessors.push(cover.format([
             {reporter: 'html'},
             {reporter: 'lcov'}
         ]));
     });
 
     gulp.task('report', function () {
-        return streamProcessors.push(gulp.dest('reports'));
+        streamProcessors.push(gulp.dest('reports'));
     });
 
     gulp.task('docjs2md', function () {
         streamProcessors.push(concat("README.md"));
-        streamProcessors.push(docjs2md({template: fs.readFileSync("docjs2md/README.hbs", "utf8")})
-                .on('error', function (err) {
-                    gutil.log("jsdoc2md failed:", err.message);
-                })
-        );
+        streamProcessors.push(docjs2md({template: fs.readFileSync("docjs2md/README.hbs", "utf8")}));
         streamProcessors.push(gulp.dest("."));
     })
 
     /************* TASKS ******************/
     gulp.task('test', ['unit', 'mocha', 'process']);
     gulp.task('cover', ['unit', 'instrument', 'mocha', 'gather', 'format', 'report', 'enforce', 'process'], function () {
-        return runSequence(['lcov', 'coveralls','exit','process']);
+        return runSequence(['lcov','expect:lcov','coveralls', 'exit', 'process']);
     });
     gulp.task('doc', ['src', 'docjs2md', 'process']);
     /**************************************/
